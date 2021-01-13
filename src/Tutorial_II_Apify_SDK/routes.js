5860ab54337dfff77097c1fc12fc884b6853bbdc
oodoow <oodoow@gmail.com>
2021-01-13 11:16:36 +0100

    minor fix

--- src/Tutorial_II_Apify_SDK/routes.js
+++ src/Tutorial_II_Apify_SDK/routes.js
@@ -7,14 +7,11 @@ const idLimit = 10;
 
 exports.handleStart = async ({ request, $ }) => {
     const requestQueue = await Apify.openRequestQueue();
+    const links = [];
     try
     {
-        //get all product links
-        const trylinks = $('div[data-asin] a.a-link-normal.a-text-normal').map(function ()
-        { return $(this).attr('href'); }).get().filter(x => x.match(/.*\/dp\/.*\//));
-        log.info('links', trylinks);
         //get all product links, transform to right regex pattern, remove duplicates
-        const links = [... new Set($('div[data-asin] a.a-link-normal.a-text-normal').map(function ()
+        links = [... new Set($('div[data-asin] a.a-link-normal.a-text-normal').map(function ()
         { return $(this).attr('href'); }).get().filter(x => x.match(/.*\/dp\/.*\//)).map(x => x.match(/.*\/dp\/.*\//)[0]))];
     }
     catch (error)
